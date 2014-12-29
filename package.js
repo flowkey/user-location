413c88fdc6ae2e64319682e06afb3800b8a09aa9
Ahmed Hassan <a.hassan@buismed.de>
2014-12-29 02:16:18 +0100

    added support for https?

--- package.js
+++ package.js
@@ -2,12 +2,15 @@ Package.describe({
   name: 'flowkey:user-location',
   summary: 'Reactive user location for free',
   version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  git: 'https://github.com/flowkey/user-location.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
+  api.use(["http"],"server");
   api.use(["reactive-var"], "client");
+  api.use(["gadicohen:headers@0.0.26"]);
   api.addFiles('user-location.js', "client");
+  api.addFiles('server.js', "server");
   api.export("UserLocation");
 });
--- user-location.js
+++ user-location.js
@@ -2,9 +2,26 @@
 UserLocation = new ReactiveVar({});
 
 Meteor.startup(function(){
+	if('https:' === document.location.protocol ){
 		$.getJSON("http://www.telize.com/geoip?callback=?",
 			function(json) {
 				UserLocation.set(json);
 			}
-		);
+		).error(function() { 
+			getLocationFromServer();
+		});
+	}else{
+		getLocationFromServer();
+	}
+
+	function getLocationFromServer(){
+		Meteor.call("getLocation", function(e, res){
+			if(!e && res){
+				console.log(res, JSON.parse(res));
+			}else{
+				console.warn("It was not possible to retrive the user location");
+			}
+		})
+	}
+
 });
