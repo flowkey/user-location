d10aad9d9d7c510e8e3efea56abed09ecd7d46b9
Christian Smorra <smorra.christian@gmail.com>
2015-09-04 12:40:47 +0200

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'flowkey:user-location',
   summary: 'Reactive user location for free',
-  version: '1.0.8',
+  version: '1.0.9',
   git: 'https://github.com/flowkey/user-location.git'
 });
 
