b4eeee674a35cfe86cb8774e9668f7e121f6d264
Marc Nitzsche <marc.nitzsche@live.de>
2017-03-17 19:19:43 +0100

    Move sanitizeResult into corresponding getLocation files, create 'UserLocation/getWithMaxMind' method

--- methods.js
+++ methods.js
@@ -14,6 +14,15 @@ Meteor.methods({
             });
     },
 
+    'UserLocation/getWithMaxMind'() {
+        const meteorConnection = this.connection;
+        return getUserIp(meteorConnection)
+            .then((userIp) => getLocationForIpWithMaxMind(userIp))
+            .catch(error => {
+                throw new Meteor.Error('[user-location]', error);
+            });
+    },
+
     'UserLocation/getUserIp'() {
         const meteorConnection = this.connection;
         return getUserIp(meteorConnection)
@@ -29,8 +38,6 @@ Meteor.methods({
     'UserLocation/getForIpWithMaxMind'(ip, sanitizeResult) {
         return getLocationForIpWithMaxMind(ip, sanitizeResult)
             .catch(error => {
-                // I don't understand this error object yet. I want to display the api errors, but am
-                // not able to do that right now?!
                 throw new Meteor.Error('[user-location]', error);
             });
     },
